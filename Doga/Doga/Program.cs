8579a2a72cc8c3be71917645b6d78b318d2a6040
BarcziBence <94312075+BarcziBence@users.noreply.github.com>
2024-10-07 10:31:58 +0200

    Add files via upload

---
+++ Doga/Doga/Program.cs
@@ -0,0 +1,54 @@
+﻿using Doga;
+
+List<Versenyzo> Versenyzok = new();
+
+StreamReader sr = new StreamReader("forras.txt");
+
+while (!sr.EndOfStream)
+{
+    Versenyzok.Add(new(sr.ReadLine()));
+}
+
+Console.WriteLine($"A versenyt {Versenyzok.Count} versenyző fejezte be");
+
+//LINQ
+// 1
+Console.WriteLine("\n 1. Feladat:");
+var elitVersenyzok = Versenyzok.Where(v => v.Kategoria == "elit").Count();
+Console.WriteLine($"{elitVersenyzok} db versenyző versenyzett elit kategóriában");
+
+// 2
+Console.WriteLine("\n 2. Feladat:");
+//var eletkorAtlagNok = Versenyzok.Where(v => v.Nem == 'n').Average(v => DateTime.Now.Year - v.SzulEv);
+//Console.WriteLine($"A női versenyzők átlag életkora: {eletkorAtlagNok}");
+
+// 3
+Console.WriteLine("\n 3. Feladat:");
+var osszKerekpar = Versenyzok.Sum(v => v.Kerekpar.TotalSeconds)/3600;
+Console.WriteLine("A versenyzők összesen {0:0.00} órát töltöttek kerékpározással", osszKerekpar);
+
+// 4
+Console.WriteLine("\n 4. Feladat:");
+var elitJAtlagUszas = TimeSpan.FromSeconds(Versenyzok.Where(v => v.Kategoria == "elit junior").Average(v => v.Uszas.TotalSeconds));
+Console.WriteLine($"Az elit junior kategóriában úszással töltött idő átlaga : {elitJAtlagUszas}");
+
+// 5
+//Console.WriteLine("\n 5. Feladat:");
+//var ferfiGyoztes = Versenyzok.Where(v => v.Nem == 'f').Min(v => v.Uszas.TotalSeconds + v.ElsoDepo.TotalSeconds + v.Kerekpar.TotalSeconds + v.MasodikDepo.TotalSeconds + v.Futas.TotalSeconds);
+//Console.WriteLine(ferfiGyoztes);
+
+// 6
+Console.WriteLine("\n 6. Feladat:");
+var kategoriaSzerint = Versenyzok.GroupBy(v => v.Kategoria);
+foreach(var a in kategoriaSzerint)
+{
+    Console.WriteLine($"{a.Key} : {a.Count()}");
+}
+
+// 7
+Console.WriteLine("\n 7. Feladat:");
+var kategoriaSzerintAtlag = Versenyzok.GroupBy(v => v.Kategoria);
+foreach (var a in kategoriaSzerint)
+{
+    Console.WriteLine($"{a.Key} : {TimeSpan.FromSeconds(a.Sum(v => v.ElsoDepo.TotalSeconds) + a.Sum(v => v.MasodikDepo.TotalSeconds))}");
+}
---
+++ Doga/Doga/Versenyzo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doga
+{
+    internal class Versenyzo
+    {
+
+        public string Nev { get; set; }
+        public int SzulEv { get; set; }
+        public int Rajtszam { get; set; }
+        public char Nem { get; set; }
+        public string Kategoria { get; set; }
+        public TimeSpan Uszas { get; set; }
+        public TimeSpan ElsoDepo { get; set; }
+        public TimeSpan Kerekpar { get; set; }
+        public TimeSpan MasodikDepo { get; set; } 
+        public TimeSpan Futas {  get; set; }
+
+        public Versenyzo(string r)
+        {
+            string[] split = r.Split(';');
+            Nev = split[0];
+            SzulEv = int.Parse(split[1]);
+            Rajtszam = int.Parse(split[2]);
+            Nev = split[3];
+            Kategoria = split[4];
+            Uszas = TimeSpan.Parse(split[5]);
+            ElsoDepo = TimeSpan.Parse(split[6]);
+            Kerekpar = TimeSpan.Parse(split[7]);
+            MasodikDepo = TimeSpan.Parse(split[8]);
+            Futas = TimeSpan.Parse(split[9]);
+        }
+
+        public override string ToString()
+        {
+            return 
+                $" Név: {Nev}, " +
+                $"Születési év:{SzulEv}, " +
+                $"Rajtszam:{Rajtszam}, Nem:{Nem}, " +
+                $"Kategória:{Kategoria}" +
+                $"Úszassal töltött idő : {Uszas}" +
+                $"Első Depóban töltött idő: {ElsoDepo}" +
+                $"Kerékpározással töltött idő: {Kerekpar}" +
+                $"Második Depóban töltött idő: {MasodikDepo}" +
+                $"Futással töltött idő: {Futas}";
+        }
+    }
+}
